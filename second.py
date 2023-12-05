5617178f09587aff9d772a08b1ef5bce45625c70
SasinduChanakaPiyumal <146552840+SasinduChanakaPiyumal@users.noreply.github.com>
2023-12-05 12:55:22 +0530

    Add files via upload

---
+++ second.py
@@ -0,0 +1,26 @@
+score = input("Enter Score: ")
+try:
+    score = float(score)
+  #  if score > 0 ||  score < 10
+except:
+    print("Enter score in number format")
+    quit()
+if score < 0 :
+    print ('The score should between o & 10')
+if score > 10 :
+    print ('The score should between o & 10')
+elif score >= 0.9 :
+    print ('A')
+elif score >= 0.8 :
+    print ('B')
+elif score >= 0.7 :
+    print('C')
+elif score >= 0.6 :
+    print('D')
+elif score < 0.6:
+    print('F')
+
+
+
+
+    
